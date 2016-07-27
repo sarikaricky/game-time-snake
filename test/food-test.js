fa61570bf9f9ad2c131a1d4a17fddd56d34fab7c
swanie21 <kswanie21@gmail.com>
2016-07-27 16:06:31 -0600

    include Math.random with food placement

--- test/food-test.js
+++ test/food-test.js
@@ -4,16 +4,19 @@ const Food = require('../lib/food');
 
 describe('Food', function() {
   context('assigned attributes', function() {
-  it('should create a piece of food', function () {
-    var food = new Food();
-    assert.isObject(food);
+    it('should create a piece of food', function () {
+      var food = new Food();
+      assert.isObject(food);
     });
-  it('should have a width and height', function () {
-    var food = new Food(30, 30, 30, 30);
-    assert.equal(food.height, 30);
-    assert.equal(food.width, 30);
-    assert.isNumber(food.x, 30);
-    assert.isNumber(food.y, 30);
+    it('should have a random x and y', function () {
+      var food = new Food(30, 45);
+      assert.equal(food.x, 30);
+      assert.equal(food.y, 45);
+    });
+    it('should have a width and height', function () {
+      var food = new Food(30, 45, 30, 30);
+      assert.equal(food.height, 30);
+      assert.equal(food.width, 30);
     });
   });
 });
--- test/world-test.js
+++ test/world-test.js
@@ -4,14 +4,14 @@ const World = require('../lib/world');
 
 describe('World', function() {
   context('assigned attributes', function() {
-  it('should create a world that the snake lives in', function () {
-    var world = new World();
-    assert.isObject(world);
+    it('should create a world that the snake lives in', function () {
+      var world = new World();
+      assert.isObject(world);
     });
-  it('should have a width and height', function () {
-    var world = new World(400, 400);
-    assert.equal(world.height, 400);
-    assert.equal(world.width, 400);
+    it('should have a width and height', function () {
+      var world = new World(400, 400);
+      assert.equal(world.height, 400);
+      assert.equal(world.width, 400);
     });
   });
 });
