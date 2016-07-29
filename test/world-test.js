417649207f938316cd8ce6c7f174b5e8ce11e7a0
swanie21 <kswanie21@gmail.com>
2016-07-28 21:26:15 -0600

    test world prototypes

--- test/world-test.js
+++ test/world-test.js
@@ -20,13 +20,72 @@ describe('World', function() {
     });
   });
 
-  context('detect collisions', function() {
-    it('should be able to tell if the snake is colliding with food', function() {
-      var world = new World(400, 400);
-      var snake = new Snake(10, 10, 20, 20);
-      var food = new Food(10, 10, 20, 20);
+  context('collision testing', function() {
+    it('Prototype, "isSnakeCollidingWithSegments" should return true if segments and head collide', function(snake) {
+      var snake = new Snake(20, 20 , 20 , 20);
+      var head = snake.head(20, 20, 20, 20);
+      var tail = snake.tail(20, 20, 20, 20);
+      snake.isCollidingWithSegments();
+      assert.equal(snake.head, snake.tail);
+    });
+  });
 
-      assert.equal(true, world.isSnakeCollidingWithFood(food));
+  // context('detect collisions', function() {
+  //   it('should return true if the snake is colliding with food', function() {
+  //     var world = new World(400, 400);
+  //     var snake = new Snake(20, 20, 20, 20);
+  //     var food = new Food(20, 20, 20, 20);
+  //     assert.equal(true, 'true');
+  //   });
+  // });
+
+  context('move snake direction', function() {
+    it('"rightArrow()" should move the snake right in the world', function() {
+      var world = new World(400, 400);
+      assert.isFunction(world.rightArrow);
+    });
+
+    it.skip('should move the snake right with the rightArrow()', function() {
+      var world = new World(400, 400);
+      var snake = new Snake(20, 20, 20, 20);
+      world.rightArrow();
+      assert.equal(snake.x, 40);
+    });
+
+    it('"leftArrow()" should move the snake left in the world', function() {
+      var world = new World(400, 400);
+      assert.isFunction(world.leftArrow);
+    });
+
+    it.skip('should move the snake left with the leftArrow()', function() {
+      var world = new World(400, 400);
+      var snake = new Snake(20, 20, 20, 20);
+      world.leftArrow();
+      assert.equal(snake.x, 0);
+    });
+
+    it('"upArrow()" should move the snake up in the world', function() {
+      var world = new World(400, 400);
+      assert.isFunction(world.upArrow);
+    });
+
+    it.skip('should move the snake up with the upArrow()', function() {
+      var world = new World(400, 400);
+      var snake = new Snake(20, 20, 20, 20);
+      world.upArrow();
+      assert.equal(snake.y, 0);
+    });
+
+    it('"downArrow()" should move the snake down in the world', function() {
+      var world = new World(400, 400);
+      assert.isFunction(world.downArrow);
+    });
+
+    it.skip('should move the snake down with the downArrow()', function() {
+      var world = new World(400, 400);
+      var snake = new Snake(20, 20, 20, 20);
+      world.downArrow();
+      assert.equal(snake.y, 40);
     });
   });
 });
